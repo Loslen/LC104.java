1d04f9653cb3ad579929def6629a8ef82976f09e
Loslen <113829585+Loslen@users.noreply.github.com>
2026-01-29 22:42:40 +0800

    Create LC226.java

---
+++ LC226.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) {
+            return null;
+        }
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
